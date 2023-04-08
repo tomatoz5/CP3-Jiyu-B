1af956f4981eade5dfaf8906ff77dfb75f52fcc6
tomatoz5 <manjirayu1995@gmail.com>
2023-04-08 18:03:39 +0700

    add exercise4
    add ex4

---
+++ Exercise4_Jirayu_B.py
@@ -0,0 +1,12 @@
+#
+FdE = 100
+GB = 80.75
+GB = 75.22
+ICS = 99.98
+CP = 100
+print("---YOUR SCORE---")
+print("Foundation English :"+(str(FdE)))
+print("General Business :"+(str(GB)))
+print("Introduction to Computer Systems :"+(str(ICS)))
+print("computer Programing :" + (str(CP)))
+print("----------------")
