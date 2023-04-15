22f8ab5674d77c6fe303cf4ae981da14b3869cef
tomatoz5 <manjirayu1995@gmail.com>
2023-04-16 00:11:34 +0700

    homework
    update

---
+++ Lecture50_Jirayu_B.py
@@ -0,0 +1,13 @@
+def sayPLUS(x,y):
+    print("PLUS",x+y)
+def sayminus(x,y):
+   print("MINUS",x-y)
+def sayMultiply(x,y):
+    print("MULTIPLY",x*y)
+def sayDivice(x,y):
+    print("DIVIDE",x/y)
+
+sayPLUS(10,5)
+sayminus(10,5)
+sayMultiply(10,5)
+sayDivice(10,5)
